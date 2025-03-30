77995ca88edd0564547af285935841f46086301b
kapil <kapilkeer1506@gmail.com>
2025-03-31 00:49:13 +0530

    build Schema for tweet

---
+++ Backend/models/Tweets.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+
+const tweetSchema=new mongoose.Schema({
+    description:{
+        type:String,
+    },
+    likes:{
+        type:Array,  //to store likes and return the length of array
+       default:[]
+    },
+    bookmarks:{
+        type:Array,   //to store of all userid who save this tweet 
+       default:[]  
+
+    },
+    userId:{
+        type:mongoose.Schema.ObjectId,  // userid of that person wwho create new tweet 
+        ref:"User"                       // refer the model of that person
+    }
+
+
+
+},
+{
+    timestamps:true
+}
+)
+const newTweet=mongoose.model("Tweet",tweetSchema);  // tweet model   
