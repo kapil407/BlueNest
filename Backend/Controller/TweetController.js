e828cd54322781d8b1b582b2af4e95905f5afc99
kapil <kapilkeer1506@gmail.com>
2025-04-01 12:36:07 +0530

    Build tweet delete feature using tweetId

--- Backend/Controller/TweetController.js
+++ Backend/Controller/TweetController.js
@@ -6,8 +6,9 @@ export const createTweetController=async (req,res)=>{
      const {description}=req.body;
      const userId=req.userId;
      const user=req.user;
-    //  console.log(description +"kshdhdsh "+ _id);
-    console.log("_id->"+userId);
+ 
+    // console.log("_id->"+userId);
+
      if(!description.length){
          return res.status(400).json({message:"write down tweet"});
      }
@@ -25,4 +26,25 @@ export const createTweetController=async (req,res)=>{
         return res.status(401).json({message:error.message});
    }
 
+}
+
+export const deleteTweetController=async (req,res)=>{
+      try {
+
+        const {id}=req.params;
+        if(!id){
+          return res.status(400).json({message:"tweet id id requird to delete tweet"});
+        }
+       
+
+          const deleteTweet=await Tweet.findByIdAndDelete(id);
+          if(!deleteTweet){
+            return res.status(401).json({message:"delete is not deleted"})
+          }
+         
+          return res.json({message:"tweet delete successfully and Deleted tweet is :",data:deleteTweet});
+
+      } catch (error) {
+          return res.status(401).json({message:error.message});
+      }
 }
