9769640b4dbf361bfcd3ebc6ba89f3a099b8931c
kapil <kapilkeer1506@gmail.com>
2025-04-01 12:36:55 +0530

    create deletetweet API

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,21 +1,22 @@
 import { validationSignUp } from "../utils/signUpValidation.js";
 import express from "express";
-
-
 import loginValidation from '../utils/loginValidation.js'
 import isAuthentication from '../Middleware/Authentication.js'
-
 import  {signUpController,LoginController,LogOutController} from "../Controller/UserController.js";
 import { createTweetController } from "../Controller/TweetController.js";
+import {deleteTweetController} from '../Controller/TweetController.js'
+
 
 const signUpRouter=express.Router();
 const loginRouter=express.Router();
 const logOutRouter=express.Router();
 const TweetRouter=express.Router();
+const DeleteRouter=express.Router();
 
  signUpRouter.post('/signUp', validationSignUp,signUpController); // signup API
 loginRouter.post('/login',loginValidation,LoginController); // login api
 logOutRouter.post('/logout',LogOutController);
 TweetRouter.post('/createTweet',isAuthentication ,createTweetController);
+TweetRouter.delete('/deleteTweet/:id',isAuthentication,deleteTweetController);
 
-export  {signUpRouter,loginRouter,logOutRouter,TweetRouter};
+export  {signUpRouter,loginRouter,logOutRouter,TweetRouter,DeleteRouter};
