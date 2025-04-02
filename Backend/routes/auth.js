dcbcfd3193daa5784c6d91154b7a9a0783053b97
kapil <kapilkeer1506@gmail.com>
2025-04-02 23:33:02 +0530

    getProfile,editProfile and bookmarks

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,10 +2,12 @@ import { validationSignUp } from "../utils/signUpValidation.js";
 import express from "express";
 import loginValidation from '../utils/loginValidation.js'
 import isAuthentication from '../Middleware/Authentication.js'
-import  {signUpController,LoginController,LogOutController} from "../Controller/UserController.js";
+import  {signUpController,LoginController,LogOutController,editProfileController} from "../Controller/UserController.js";
 import { createTweetController } from "../Controller/TweetController.js";
 import {deleteTweetController} from '../Controller/TweetController.js'
 import { likeOrDisLikeController } from "../Controller/TweetController.js";
+import { bookmarksController } from "../Controller/UserController.js";
+import { getProfileController } from "../Controller/UserController.js";
 
 
 const signUpRouter=express.Router();
@@ -14,6 +16,10 @@ const logOutRouter=express.Router();
 const TweetRouter=express.Router();
 const DeleteRouter=express.Router();
 const likeOrDisLikeRouter=express.Router();
+const editProfileRouter=express.Router();
+const bookmarksRouter=express.Router();
+const getProfileRouter=express.Router();
+
 
  signUpRouter.post('/signUp', validationSignUp,signUpController); // signup API
 loginRouter.post('/login',loginValidation,LoginController); // login api
@@ -21,5 +27,8 @@ logOutRouter.post('/logout',LogOutController);
 TweetRouter.post('/createTweet',isAuthentication ,createTweetController);
 TweetRouter.delete('/deleteTweet/:id',isAuthentication,deleteTweetController);
 likeOrDisLikeRouter.patch('/tweetLikeOrDisLike/:id',isAuthentication,likeOrDisLikeController);
+editProfileRouter.patch('/updateProfile',isAuthentication,editProfileController);
+bookmarksRouter.patch('/bookmarkstweet/:id',isAuthentication,bookmarksController);
+getProfileRouter.get('/getProfile',isAuthentication,getProfileController);
 
-export  {signUpRouter,loginRouter,logOutRouter,TweetRouter,DeleteRouter,likeOrDisLikeRouter};
+export  {signUpRouter,loginRouter,logOutRouter,TweetRouter,DeleteRouter,likeOrDisLikeRouter,editProfileRouter,bookmarksRouter,getProfileRouter};
