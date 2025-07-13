f8c1d76398cd45cbaafd8bc4de7bc776f81eadfc
kapil <kapilkeer1506@gmail.com>
2025-07-13 12:51:42 +0530

    correct

--- Backend/routes/MessageRoute.js
+++ Backend/routes/MessageRoute.js
@@ -2,8 +2,8 @@ import express from "express"
 import isAuthentication from '../Middleware/Authentication.js'
 import { sendMessage } from "../Controller/MessageController.js";
 
-const sendMessageRouter=express.Router();
+const router=express.Router();
 
-sendMessageRouter.post('/sendMessage/:id',isAuthentication,sendMessage);
+router.post('/sendMessage/:id',isAuthentication,sendMessage);
 
- export default sendMessageRouter;
+ export default router;
