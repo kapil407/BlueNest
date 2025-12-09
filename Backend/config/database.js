023d6eb64c87bd845c25e883219d3fae3f787a8d
kapil <kapilkeer1506@gmail.com>
2025-12-09 19:06:06 +0530

    fix bug

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectDB= async ()=>{
-       await mongoose.connect("mongodb+srv://kapilkeer1998:smtbKlczVkp3rIxM@kapildb.pc7gf.mongodb.net/twitter");
-}
+export const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://kapilkeer1998:smtbKlczVkp3rIxM@kapildb.pc7gf.mongodb.net/twitter"
+  );
+};
