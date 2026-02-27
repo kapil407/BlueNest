de99bddc6db8f7ca72b05f5406165b97e715a510
Kapil Keer <kapilkeer1998@gmail.com>
2026-02-27 15:58:21 +0530

    fix body.jsx

--- Frontend/src/Components/Body.jsx
+++ Frontend/src/Components/Body.jsx
@@ -4,7 +4,7 @@ const Home = lazy(() => import("./Home.jsx"));
 const Feed = lazy(() => import("./Feed.jsx"));
 const Profile = lazy(() => import("./Profile.jsx"));
 const Signup = lazy(() => import("./Signup.jsx"));
-const EditeProfile = lazy(() => import("./EditeProfile.jsx"));
+const Editeprofile = lazy(() => import("./Editeprofile.jsx"));
 const Message = lazy(() => import("./Message.jsx"));
 // const { ChatRoom } = lazy(() => import("./ChatRoom.jsx"));
 const Bookmarks = lazy(() => import("./Bookmarks.jsx"));
@@ -54,10 +54,10 @@ const Body = () => {
         },
 
         {
-          path: "/EditeProfile",
+          path: "/Editeprofile",
           element: (
             <Suspense fallback={<Spinner />}>
-              <EditeProfile />,
+              <Editeprofile />,
             </Suspense>
           ),
         },
