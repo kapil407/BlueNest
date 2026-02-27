67865d539cbdc7f1be9ecd4d42c6f22649949251
Kapil keer <kapilkeer1506@gmail.com>
2026-02-27 15:39:25 +0530

    change backend url

--- Frontend/src/Utils/constant.js
+++ Frontend/src/Utils/constant.js
@@ -1,6 +1,6 @@
-export const  USER_API_END_POINT="http://localhost:4660"
+export const  USER_API_END_POINT="https://bluenest-backend.onrender.com"
 
-export const  TWEET_API_END_POINT="http://localhost:4660"
+export const  TWEET_API_END_POINT="https://bluenest-backend.onrender.com"
 
 
 
