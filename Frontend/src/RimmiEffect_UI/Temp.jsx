8bcfb7fac529e7cec05f313e0e83cf0dc484ae09
Kapil Keer <kapilkeer1998@gmail.com>
2026-02-28 15:23:00 +0530

    FeedRimmi width update

--- Frontend/src/RimmiEffect_UI/Temp.jsx
+++ Frontend/src/RimmiEffect_UI/Temp.jsx
@@ -43,7 +43,7 @@ const FeedRemmi = () => {
               </div>
 
               {/* Post Button */}
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition">
+              <button className="bg-gray-400 text-white px-4 py-2 rounded-full  transition">
                
               </button>
 
