db0eb46529e0e12a5d76d65bd810019bd26c8cb1
Kapil Keer <kapilkeer1998@gmail.com>
2026-02-28 16:24:50 +0530

    FeedRimmi width update

--- Frontend/src/RimmiEffect_UI/Temp.jsx
+++ Frontend/src/RimmiEffect_UI/Temp.jsx
@@ -4,6 +4,13 @@ const FeedRemmi = () => {
   const {theme}=useSelector(store=>store.theme);
     return (
     <>
+   <div className="flex h-screen w-screen">
+     <div className="w-[10%] h-screen">
+      {/* leftside bar */}
+    </div>
+    <div className="w-[18%] h-screen">
+      {/* rightside bar */}
+    </div>
       <div className={`h-screen w-screen ${theme=="light"?"bg-white":"bg-black"}`}>
         
         {/* Feed Container */}
@@ -14,7 +21,7 @@ const FeedRemmi = () => {
                        ${theme=="light"?"bg-white":"bg-black"}`}>
 
           {/* Upper Box */}
-          <div className="mb-4 border-b border-gray-200 py-6 bg-gray-400">
+          <div className="mb-4 border-b border-gray-200 py-6 bg-gray-700">
 
           </div>
 
@@ -25,10 +32,10 @@ const FeedRemmi = () => {
             <div className="flex  items-start ">
               
               {/* Image */}
-              <div className="w-[83px] h-[75px] rounded-full bg-gray-300"></div>
+              <div className="w-[83px] h-[75px] rounded-full bg-gray-700"></div>
 
               {/* Input */}
-              <div className="flex-1 bg-gray-400 ml-4 mt-1">
+              <div className="flex-1 bg-gray-700 ml-4 mt-1">
                 <input
                   type="text"
                   placeholder=""
@@ -42,12 +49,12 @@ const FeedRemmi = () => {
             <div className="flex justify-between items-center mt-4">
 
               {/* Media Button */}
-              <div className="text-blue-500 cursor-pointer">
+              <div className=" cursor-pointer h-10 w-10 borderd  rounded">
                 
               </div>
 
               {/* Post Button */}
-              <button className="bg-gray-400 text-white px-4 py-2 rounded-full  transition">
+              <button className="bg-gray-400 text-white  p-3 w-24 rounded-full  transition">
                
               </button>
 
@@ -57,6 +64,7 @@ const FeedRemmi = () => {
 
         </div>
       </div>
+   </div>
     </>
   );
 };
