6d2319d9724793a39919fa96d50084b13c996af8
Kapil Keer <kapilkeer1998@gmail.com>
2026-02-28 17:30:55 +0530

    leftside bar width update

--- Frontend/src/Components/LeftSideBar.jsx
+++ Frontend/src/Components/LeftSideBar.jsx
@@ -56,7 +56,7 @@ const LeftSideBar = () => {
   const SelecetedPage = "Selected";
   return (
     <>
-      <div className="w-[22%] relative h-screen ">
+      <div className="w-[18%] relative h-screen ">
         <Link
           to={"/"}
           className="hover:bg-gray-300 hover:cursor-pointer rounded-full w-16 h-16 flex items-center justify-center transition delay-75 ease-in mt-1 mb-2 border border-gray-400 "
