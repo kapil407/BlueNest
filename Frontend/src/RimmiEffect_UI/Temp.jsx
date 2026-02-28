8912c1d9369aeb2925aacba900d99348f4d1c5b1
Kapil Keer <kapilkeer1998@gmail.com>
2026-02-28 16:00:18 +0530

    FeedRimmi width update

--- Frontend/src/RimmiEffect_UI/Temp.jsx
+++ Frontend/src/RimmiEffect_UI/Temp.jsx
@@ -7,10 +7,10 @@ const FeedRemmi = () => {
       <div className={`h-screen w-screen ${theme=="light"?"bg-white":"bg-black"}`}>
         
         {/* Feed Container */}
-        <div className={` rounded-2xl 
+        <div className={` 
                         shadow-[0_8px_30px_rgba(0,0,0,0.08)] 
                         border border-gray-200 
-                        p-4 transition duration-300 w-[55%] mx-auto mt-6
+                        p-4 transition duration-300 w-[55%] mx-auto 
                        ${theme=="light"?"bg-white":"bg-black"}`}>
 
           {/* Upper Box */}
