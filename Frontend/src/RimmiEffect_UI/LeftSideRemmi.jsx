7f4f7a112f17645bb49c77baf5cfc04a97ed7be5
Kapil Keer <kapilkeer1998@gmail.com>
2026-02-28 23:48:03 +0530

    add left and right remmmi

---
+++ Frontend/src/RimmiEffect_UI/LeftSideRemmi.jsx
@@ -0,0 +1,23 @@
+const LeftSideRemmi = () => {
+  return <>
+         <div
+          className={`w-[22%] h-screen ${theme == "light" ? "bg-white" : "bg-black"}`}
+        >
+          {/* leftside bar */}
+          <div className="w-35 p-6 bg-gray-700">
+            {/* Home */}
+          </div>
+          <div className="w-35 p-6 bg-gray-700">
+            {/* Home */}
+          </div>
+          <div className="w-35 p-6 bg-gray-700">
+            {/* Home */}
+          </div>
+          <div className="w-35 p-6 bg-gray-700">
+            {/* Home */}
+          </div>
+        </div>
+
+  </>;
+};
+export default LeftSideRemmi;
---
+++ Frontend/src/RimmiEffect_UI/RightSideRemmi.jsx
@@ -0,0 +1,18 @@
+
+
+const RightSideRemmi=()=>{
+    return (
+        <>
+         <div className={`w-[22%] h-screen ${theme=="light"?"bg-white":"bg-black"}`}>
+          {/* rightside bar */}
+          <div className="w-[18%] p-6 bg-gray-700">
+            <div className="bg-gray-700 w-[18%] h-20">
+              
+            </div>
+
+          </div>
+          </div>
+        </>
+    )
+}
+export default RightSideRemmi ;
