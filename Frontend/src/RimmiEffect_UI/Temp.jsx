fb0fc1fc619b27c0da9a2f9d15e7adf8bc5bdafb
Kapil Keer <kapilkeer1998@gmail.com>
2026-02-28 17:55:43 +0530

    FeedRimmi width update

--- Frontend/src/RimmiEffect_UI/Temp.jsx
+++ Frontend/src/RimmiEffect_UI/Temp.jsx
@@ -45,7 +45,7 @@ const FeedRemmi = () => {
               </div>
 
               {/* Media + Button */}
-              <div className="flex justify-between items-center p-8 ">
+              <div className="flex justify-between items-center p-8 h-[8%]">
                 {/* Media Button */}
                 <div className=" cursor-pointer h-8 w-8 borderd bg-gray-700 mb-6 rounded"></div>
 
@@ -57,49 +57,7 @@ const FeedRemmi = () => {
             {/* </div> */}
           </div>
           <div className="border-b border-gray-200 w-[70%]">
-            <div className="w-full">
-              <div className="flex flex-col">
-                <div className="ml-1 flex  items-center">
-                  <div className="">
-                    <div className="flex ml-6">
-                      <button className="m-1  ">
-                        <img
-                          alt="photo"
-                          className="w-13 h-13 rounded-full border-2 object-cover border-gray-400 "
-                        />
-                      </button>
-                      <div className="flex ml-2 mb-4 items-center ">
-                        <h1 className="font-bold text-lg ml-1"></h1>
-                        <p className="ml-1"></p>
-                      </div>
-                    </div>
-
-                    <div className=" w-[90%] flex justify-center items-center ml-12 ">
-                      <div>
-                        <img
-                          alt="image"
-                          className=" rounded object-cover w-180 h-120 "
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between p-8">
-                  <div className="flex   p-2 rounded-full  cursor-pointer"></div>
-                  <div className="flex  p-2 rounded-full cursor-pointer">
-                    <button className="flex"></button>
-                  </div>
-                  <div>
-                    <button className="flex   p-2 rounded-full  cursor-pointer"></button>
-
-                    <>
-                      <button className=" flex items-center   rounded-3xl p-2"></button>
-                    </>
-                  </div>
-                </div>
-              </div>
-            </div>
+         
           </div>
         </div>
         <div className="w-[22%] h-screen">{/* rightside bar */}</div>
