1784498efe7d17b83018c2e61e5cb2892ebd1eee
Kapil Keer <kapilkeer1998@gmail.com>
2026-02-28 16:33:31 +0530

    FeedRimmi width update

--- Frontend/src/RimmiEffect_UI/Temp.jsx
+++ Frontend/src/RimmiEffect_UI/Temp.jsx
@@ -44,7 +44,7 @@ const FeedRemmi = () => {
             </div>
 
             {/* Media + Button */}
-            <div className="flex justify-between items-center mt-4">
+            <div className="flex justify-between items-center p-8">
 
               {/* Media Button */}
               <div className=" cursor-pointer h-10 w-10 borderd  rounded">
