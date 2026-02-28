4e1406421b7d85468f29cce625875766c91a67db
Kapil Keer <kapilkeer1998@gmail.com>
2026-03-01 01:00:04 +0530

    left and right remmmi

--- Frontend/src/RimmiEffect_UI/LeftSideRemmi.jsx
+++ Frontend/src/RimmiEffect_UI/LeftSideRemmi.jsx
@@ -8,7 +8,7 @@ const LeftSideRemmi = () => {
           className={`w-[18%] h-screen flex flex-col mt-1 gap-3 ${theme == "light" ? "bg-white" : "bg-black"}`}
         >
           {/* leftside bar */}
-          <div className={`w-20 p-6 rounded-full  ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
+          <div className={`w-15 p-6 rounded-full  ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
             {/* Home */}
           </div>
           <div className={`w-35 p-6 rounded-full  ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
