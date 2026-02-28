c4d02c8da970e9b992125864ceeb90b986257789
Kapil Keer <kapilkeer1998@gmail.com>
2026-03-01 01:24:32 +0530

    add left and right remmmi

--- Frontend/src/RimmiEffect_UI/RightSideRemmi.jsx
+++ Frontend/src/RimmiEffect_UI/RightSideRemmi.jsx
@@ -8,8 +8,8 @@ const RightSideRemmi = () => {
         className={`w-[18%] flex flex-col  h-screen ${theme == "light" ? "bg-white" : "bg-black"}`}
       >
         {/* rightside bar */}
-        <div className={` p-6  p-3 mb-2${theme=="light"?"bg-gray-400":"bg-gray-700"}`}></div>
-        <div className={`  h-20 mt-2 ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}></div>
+        <div className={` p-6 w-[100%]  p-3 mb-2${theme=="light"?"bg-gray-400":"bg-gray-700"}`}></div>
+        <div className={` w-[100%] h-20 mt-2 ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}></div>
       </div>
     </>
   );
