767bfbbd6140488ad189219526341e21a5f7fb34
Kapil Keer <kapilkeer1998@gmail.com>
2026-03-01 10:50:13 +0530

    leftside bar width update

--- Frontend/src/RimmiEffect_UI/LeftSideRemmi.jsx
+++ Frontend/src/RimmiEffect_UI/LeftSideRemmi.jsx
@@ -2,25 +2,26 @@ import { useSelector } from "react-redux";
 import store from "../redux/store";
 
 const LeftSideRemmi = () => {
+  
   const {theme}=useSelector(store=>store.theme);
   return <>
          <div
           className={`w-[18%] h-screen flex flex-col mt-1 gap-3 ${theme == "light" ? "bg-white" : "bg-black"}`}
         >
           {/* leftside bar */}
-          <div className={`w-15 p-6 rounded-full mb-4 ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
+          <div className={`w-15 p-6 rounded-full animate-pulse mb-4 ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
             {/* Home */}
           </div>
-          <div className={`w-35 p-6 rounded-full mt-2 ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
+          <div className={`w-35 p-6 rounded-full animate-pulse mt-2 ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
             {/* Home */}
           </div>
-          <div className={`w-35 p-6 rounded-full  ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
+          <div className={`w-35 p-6 rounded-full animate-pulse  ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
             {/* Home */}
           </div>
-          <div className={`w-35 p-6 rounded-full  ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
+          <div className={`w-35 p-6 rounded-full animate-pulse  ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
             {/* Home */}
           </div>
-            <div className={`w-35 p-6 rounded-full  ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
+            <div className={`w-35 p-6 rounded-full animate-pulse ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}>
             {/* Home */}
           </div>
         </div>
