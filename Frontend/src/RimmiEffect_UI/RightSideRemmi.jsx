a4ac3de8518c4c0e0067eaac6ccc1cef1dfde55a
Kapil Keer <kapilkeer1998@gmail.com>
2026-03-01 10:39:30 +0530

    right remmmi

--- Frontend/src/RimmiEffect_UI/RightSideRemmi.jsx
+++ Frontend/src/RimmiEffect_UI/RightSideRemmi.jsx
@@ -8,8 +8,8 @@ const RightSideRemmi = () => {
         className={`w-[18%] flex flex-col  h-screen ${theme == "light" ? "bg-white" : "bg-black"}`}
       >
         {/* rightside bar */}
-        <div className={` p-6 w-25 rounded-full  p-2 mb-2 mt-1 ${theme=="light"?"bg-gray-400 border-gray-400":"bg-gray-700 border-gray-700"}`}></div>
-        <div className={`  h-20 w-25 mt-2 ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}></div>
+        <div className={` p-6 w-45 rounded-full  p-2 mb-2 mt-1 ${theme=="light"?"bg-gray-400 border-gray-400":"bg-gray-700 border-gray-700"}`}></div>
+        <div className={`  h-30 w-45 mt-2 ${theme=="light"?"bg-gray-400":"bg-gray-700"}`}></div>
       </div>
     </>
   );
