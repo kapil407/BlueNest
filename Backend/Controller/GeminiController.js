4ffeeb97b378419cb6ff287634a78ac1df1db929
Kapil Keer <kapilkeer1998@gmail.com>
2026-03-02 16:48:38 +0530

    check api_key by console

--- Backend/Controller/GeminiController.js
+++ Backend/Controller/GeminiController.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 // import User from "../models/User.js";
 import dotenv from "dotenv";
 dotenv.config();
-
+console.log("api_key",process.env.GEMINI_API_KEY);
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const GeneratePostText = async (req, res) => {
@@ -10,7 +10,7 @@ const GeneratePostText = async (req, res) => {
     const { prompt } = req.body;
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash"
+      model: "gemini-1.5-flash",
     });
     const result = await model.generateContent(
       `Generate a short social media post. Keep it under 150 words.\nTopic: ${prompt}`,
@@ -18,8 +18,8 @@ const GeneratePostText = async (req, res) => {
 
     const response = await result.response;
     const text = response.text();
-    const user=req.user;
-    user.aiUsageToday += 1;
+    const user = req.user;
+    user.aiUsageToday = (user.aiUsageToday || 0) + 1;
     await user.save();
 
     res.status(200).json({
