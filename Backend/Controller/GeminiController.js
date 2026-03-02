84192edb7d468277a22509e8d2d089c723dfb275
Kapil Keer <kapilkeer1998@gmail.com>
2026-03-02 19:12:16 +0530

    change gemini  model

--- Backend/Controller/GeminiController.js
+++ Backend/Controller/GeminiController.js
@@ -11,7 +11,7 @@ const GeneratePostText = async (req, res) => {
     const { prompt } = req.body;
 
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-flash",
+     model: "gemini-2.0-flash",
       contents: `Generate a short social media post under 150 words.
       Topic: ${prompt}`,
     });
