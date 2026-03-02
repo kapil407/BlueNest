89cd508dd67bd244445f755c8856ffd2972c651d
Kapil Keer <kapilkeer1998@gmail.com>
2026-03-02 15:17:32 +0530

    routes

--- Backend/routes/GeminiRoutes.js
+++ Backend/routes/GeminiRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-const router=express.Router();
-import GeneratePostText from "../Controller/GeminiController";
-import isAuthentication from "../Middleware/Authentication";
-import aiLimiter from "../Middleware/PromptRateLimit";
-import GeminiPromptValidation from "../Middleware/PromptValidation";
-router.post("/generate-post",isAuthentication,aiLimiter,GeminiPromptValidation, GeneratePostText);
-export default router ;
+const router = express.Router();
+import GeneratePostText from "../Controller/GeminiController.js";
+import isAuthentication from "../Middleware/Authentication.js";
+import aiLimiter from "../Middleware/PromptRateLimit.js";
+import GeminiPromptValidation from "../Middleware/PromptValidation.js";
+router.post(
+  "/generate-post",
+  isAuthentication,
+  aiLimiter,
+  GeminiPromptValidation,
+  GeneratePostText,
+);
+export default router;
