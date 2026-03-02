d498d45c1981779adef1d036168230bb62444201
Kapil Keer <kapilkeer1998@gmail.com>
2026-03-02 18:28:27 +0530

    change gemini version

--- Backend/Controller/GeminiController.js
+++ Backend/Controller/GeminiController.js
@@ -10,7 +10,7 @@ const GeneratePostText = async (req, res) => {
     const { prompt } = req.body;
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-pro",
+     model: "gemini-1.5-flash",
     });
     const result = await model.generateContent(
       `Generate a short social media post. Keep it under 150 words.\nTopic: ${prompt}`,
