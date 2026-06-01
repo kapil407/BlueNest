0ef80c9dabd384d521c9e4447db81a9f4ab9f1de
Kapil Keer <kapilkeer1998@gmail.com>
2026-06-01 23:19:27 +0530

    try to fix nodemailer issue

--- Backend/Controller/UserController.js
+++ Backend/Controller/UserController.js
@@ -22,8 +22,7 @@ const generateOTP = () => crypto.randomInt(10000, 100000);
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+ 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
