3471291917c2bc07800960a27b0ad31769935bff
Kapil Keer <kapilkeer1998@gmail.com>
2026-06-02 17:55:10 +0530

    try to implement token rotation

--- Backend/GenerateTokens/Tokens.js
+++ Backend/GenerateTokens/Tokens.js
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 dotenv.config();
- const AccessToken=(userId)=>{
-  return    await jwt.sign({userId},process.env.AccessToken_Secret_Key,{expiresIn:'15m'});
+ const AccessToken= (userId)=>{
+  return     jwt.sign({userId},process.env.AccessToken_Secret_Key,{expiresIn:'15m'});
  }
- const RefreshToken=(userId)=>{
-   return   await jwt.sign({userId},process.env.RefreshToken_Secret_Key,{expiresIn:'7d'});
+ const RefreshToken= (userId)=>{
+   return   jwt.sign({userId},process.env.RefreshToken_Secret_Key,{expiresIn:'7d'});
  }
  export default {AccessToken,RefreshToken};
