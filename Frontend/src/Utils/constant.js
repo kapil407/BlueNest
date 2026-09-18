b155878012fc013988db24320191fe96a7b7e0a8
Kapil Keer <kapilkeer1998@gmail.com>
2026-09-18 22:54:19 +0530

    change url from localhost to render

--- Frontend/src/Utils/constant.js
+++ Frontend/src/Utils/constant.js
@@ -1,8 +1,8 @@
 
-export const  USER_API_END_POINT="http://localhost:4660" || "https://bluenest-backend.onrender.com" ;
+export const  USER_API_END_POINT= "https://bluenest-backend.onrender.com" ;
     
 
-export const  TWEET_API_END_POINT="http://localhost:4660" || "https://bluenest-backend.onrender.com";
+export const  TWEET_API_END_POINT= "https://bluenest-backend.onrender.com";
 
 
 
