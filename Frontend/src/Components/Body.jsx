5ec0633faa3004aeb3f0e44c668f44a936449a49
Kapil Keer <kapilkeer1998@gmail.com>
2026-09-18 18:21:22 +0530

    create logging page after passport identified user

--- Frontend/src/Components/Body.jsx
+++ Frontend/src/Components/Body.jsx
@@ -13,85 +13,59 @@ const OtpVerify = lazy(() => import("./OtpVerify.jsx"));
 const EditeProfile = lazy(() => import("./Editeprofile.jsx"));
 const Message = lazy(() => import("./Message.jsx"));
 const Bookmarks = lazy(() => import("./Bookmarks.jsx"));
-const ChangeEmailAndPassword = lazy(() => import("./ChangeEmailAndPasswor.jsx"));
+const ChangeEmailAndPassword = lazy(
+  () => import("./ChangeEmailAndPasswor.jsx"),
+);
 const NotificationComponent = lazy(() => import("./Notification.jsx"));
 const SettingComponent = lazy(() => import("./Setting.jsx"));
-const DeleteAccountComponent =lazy(()=>import('./DeleteAccount.jsx'));
+const DeleteAccountComponent = lazy(() => import("./DeleteAccount.jsx"));
 
 import ChatBotComponent from "./Chatbot.jsx";
-
+import GoogleSuccess from "./GoogleSucess.jsx";
 
 const Body = () => {
-
-//   const navigate=useNavigate();
-//   const location = useLocation();
-//   const generateNewAccessToken = useGenerateAccessToken();
-//   const { user } = useSelector((store) => store.user);
-
-//   useEffect(() => {
-//   const publicPaths = ["/login", "/signup", "/otpverify", "/auth/google/success"];
-//   const currentPath = location.pathname.toLowerCase();
-
-//   if (publicPaths.includes(currentPath)) {
-//     return;
-//   }
-
-//   const refreshAccessToken = async () => {
-//     const status = await generateNewAccessToken();
-
-//     if (status === 401) {
-//       navigate("/login");
-//     }
-//   };
-
-//   refreshAccessToken();
-
-//   const intervalId = setInterval(() => {
-//     generateNewAccessToken();
-//   }, 14 * 60 * 1000);
-
-//   return () => clearInterval(intervalId);
-// }, [location.pathname,navigate]);
-
+  
   return (
-   
-      <Suspense fallback={<Spinner />}>
-        <Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<FeedRemmi />}>
+              <Home />
+            </Suspense>
+          }
+        >
           <Route
-            path="/"
+            index
             element={
               <Suspense fallback={<FeedRemmi />}>
-                <Home />
+                <Feed />
               </Suspense>
             }
-          >
-            <Route
-              index
-              element={
-                <Suspense fallback={<FeedRemmi />}>
-                  <Feed />
-                </Suspense>
-              }
-            />
+          />
 
-            <Route path="profile/:id" element={<Profile />} />
-            <Route path="/profile/EditeProfile" element={<EditeProfile />} />
-            <Route path="Message/:targetUserId" element={<Message />} />
-            <Route path="bookmarks/:id" element={<Bookmarks />} />
-            <Route path="settings/:id" element={<ChangeEmailAndPassword />} />
-            <Route path="notifications/:id" element={<NotificationComponent />} />
-            <Route path="/setting" element={<SettingComponent />} />
-            <Route path="/setting/deleteAccount" element={<DeleteAccountComponent/>} />
-            <Route path="Chat-Bot" element={<ChatBotComponent/>}/>
-          </Route> 
-            {/* {end of Home Route in which all sub-routes are defined like Feed,profile,editeProfile,Message,Bookmarks } */}
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/auth/google/success" element={<GoogleAuthSuccess />} />
-          <Route path="/otpVerify" element={<OtpVerify />} />
-        </Routes>
-      </Suspense>
-
+          <Route path="profile/:id" element={<Profile />} />
+          <Route path="/profile/EditeProfile" element={<EditeProfile />} />
+          <Route path="Message/:targetUserId" element={<Message />} />
+          <Route path="bookmarks/:id" element={<Bookmarks />} />
+          <Route path="settings/:id" element={<ChangeEmailAndPassword />} />
+          <Route path="notifications/:id" element={<NotificationComponent />} />
+          <Route path="/setting" element={<SettingComponent />} />
+          <Route
+            path="/setting/deleteAccount"
+            element={<DeleteAccountComponent />}
+          />
+          <Route path="Chat-Bot" element={<ChatBotComponent />} />
+        </Route>
+        {/* {end of Home Route in which all sub-routes are defined like Feed,profile,editeProfile,Message,Bookmarks } */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth/google/success" element={<GoogleSuccess />} />
+        <Route path="/auth/google/success" element={<GoogleAuthSuccess />} />
+        <Route path="/otpVerify" element={<OtpVerify />} />
+      </Routes>
+    </Suspense>
   );
 };
 
---
+++ Frontend/src/Components/GoogleSucess.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+const GoogleSuccess = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, [navigate]);
+
+  return <div>Logging you in...</div>;
+};
+export default GoogleSuccess;
